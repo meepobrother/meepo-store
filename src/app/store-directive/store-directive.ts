22f94766dc118f271c80e41bd3d13ee86ec6659a
15138730370 <1037483576@qq.com>
2018-01-15 09:11:28 +0800

    save to back

--- src/app/store-directive/store-directive.ts
+++ src/app/store-directive/store-directive.ts
@@ -1,23 +1,73 @@
-import { Directive, Input, Output, EventEmitter, OnInit, ElementRef, Renderer2, HostListener } from '@angular/core';
+import {
+    Directive, Input, Output,
+    EventEmitter, OnInit, ElementRef,
+    Renderer2, HostListener,
+    forwardRef, ChangeDetectorRef,
+    OnDestroy
+} from '@angular/core';
 import { StoreService } from '../store';
-@Directive({ selector: '[store]' })
-export class StoreDirective implements OnInit {
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+@Directive({
+    selector: '[store]',
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => StoreDirective),
+            multi: true
+        }
+    ]
+})
+export class StoreDirective implements ControlValueAccessor, OnDestroy, OnInit {
     @Input() store: string;
-    @Output() storeChange: EventEmitter<any> = new EventEmitter();
+    _model: any;
+    set model(val) {
+        this._model = val;
+        this.propagateChange(this._model);
+    }
+    get model() {
+        return this._model;
+    }
 
     @HostListener('change', ['$evennt'])
     onChange() {
         if (this.ele) {
             if (this.isTextInput(this.ele.nativeElement)) {
-                this._store.set(this.store, this.ele.nativeElement.value);
+                this.model = this.ele.nativeElement.value;
             }
         }
     }
+
     constructor(
         public ele: ElementRef,
         public render: Renderer2,
+        public cd: ChangeDetectorRef,
         public _store: StoreService
     ) { }
+
+    ngOnInit() {
+        this.ele.nativeElement.value = this._store.get(this.store);
+        this.cd.reattach();
+    }
+
+    ngOnDestroy() {
+        this.cd.detach();
+    }
+
+    propagateChange = (_: any) => { };
+
+    writeValue(value: any) {
+        if (this.isTextInput(this.ele.nativeElement)) {
+            this._store.set(this.store, value);
+            this.registerOnTouched(value);
+        }
+    }
+
+    registerOnChange(fn: any) {
+        this.propagateChange = fn;
+    }
+
+    registerOnTouched(fn: any) { }
+
 
     isTextInput(ele: any) {
         return !!ele &&
@@ -28,12 +78,6 @@ export class StoreDirective implements OnInit {
 
     isDiv(ele: any) {
         return !!ele && (ele.tagName === 'DIV');
-    }
-
-    ngOnInit() {
-        if (this.isTextInput(this.ele.nativeElement)) {
-            this.ele.nativeElement.value = this._store.get(this.store)
-        }
     }
 }
 export const NON_TEXT_INPUT_REGEX = /^(radio|checkbox|range|file|submit|reset|color|image|button)$/i;
